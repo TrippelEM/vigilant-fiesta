7340848d3269f7a7bb35d3c148511d253123ca56
Your Name <you@example.com>
2025-10-15 16:51:43 +0200

    First commit

---
+++ bucket_game.py
@@ -0,0 +1,62 @@
+from halving_game import minimax_search
+
+State = tuple[int, list[str | int]]  # Tuple of player (whose turn it is),
+                                     # and the buckets (as str)
+                                     # or the number in a bucket
+Action = str | int  # Bucket choice (as str) or choice of number
+
+
+class Game:
+    def initial_state(self) -> State:
+        return 0, ['A', 'B', 'C']
+
+    def to_move(self, state: State) -> int:
+        player, _ = state
+        return player
+
+    def actions(self, state: State) -> list[Action]:
+        _, actions = state
+        return actions
+
+    def result(self, state: State, action: Action) -> State:
+        if action == 'A':
+            return (self.to_move(state) + 1) % 2, [-50, 50]
+        elif action == 'B':
+            return (self.to_move(state) + 1) % 2, [3, 1]
+        elif action == 'C':
+            return (self.to_move(state) + 1) % 2, [-5, 15]
+        assert type(action) is int
+        return (self.to_move(state) + 1) % 2, [action]
+
+    def is_terminal(self, state: State) -> bool:
+        _, actions = state
+        return len(actions) == 1
+
+    def utility(self, state: State, player: int) -> float:
+        assert self.is_terminal(state)
+        _, actions = state
+        assert type(actions[0]) is int
+        return actions[0] if player == self.to_move(state) else -actions[0]
+
+    def print(self, state):
+        print(f'The state is {state} and ', end='')
+        if self.is_terminal(state):
+            print(f'P1\'s utility is {self.utility(state, 0)}')
+        else:
+            print(f'it is P{self.to_move(state)+1}\'s turn')
+
+def main():
+    game = Game()
+    state = game.initial_state()
+    game.print(state)
+    while not game.is_terminal(state):
+        cur_player = game.to_move(state)
+        action = minimax_search(game, state) # The player whose turn it is
+                                             # is the MAX player
+        print(f'P{cur_player+1}\'s action: {action}')
+        assert action is not None
+        state = game.result(state, action)
+        game.print(state)
+
+if __name__ == "__main__":
+    main()
---
+++ tic_tac_toe.py
@@ -0,0 +1,122 @@
+import math
+from copy import deepcopy
+
+State = tuple[int, list[list[int | None]]]  # Tuple of player (whose turn it is),
+                                            # and board
+Action = tuple[int, int]  # Where to place the player's piece
+
+class Game:
+    def initial_state(self) -> State:
+        return (0, [[None, None, None], [None, None, None], [None, None, None]])
+
+    def to_move(self, state: State) -> int:
+        player_index, _ = state
+        return player_index
+
+    def actions(self, state: State) -> list[Action]:
+        _, board = state
+        actions = []
+        for row in range(3):
+            for col in range(3):
+                if board[row][col] is None:
+                    actions.append((row, col))
+        return actions
+
+    def result(self, state: State, action: Action) -> State:
+        _, board = state
+        row, col = action
+        next_board = deepcopy(board)
+        next_board[row][col] = self.to_move(state)
+        return (self.to_move(state) + 1) % 2, next_board
+
+    def is_winner(self, state: State, player: int) -> bool:
+        _, board = state
+        for row in range(3):
+            if all(board[row][col] == player for col in range(3)):
+                return True
+        for col in range(3):
+            if all(board[row][col] == player for row in range(3)):
+                return True
+        if all(board[i][i] == player for i in range(3)):
+            return True
+        return all(board[i][2 - i] == player for i in range(3))
+
+    def is_terminal(self, state: State) -> bool:
+        _, board = state
+        if self.is_winner(state, (self.to_move(state) + 1) % 2):
+            return True
+        return all(board[row][col] is not None for row in range(3) for col in range(3))
+
+    def utility(self, state, player):
+        assert self.is_terminal(state)
+        if self.is_winner(state, player):
+            return 1
+        if self.is_winner(state, (player + 1) % 2):
+            return -1
+        return 0
+
+    def print(self, state: State):
+        _, board = state
+        print()
+        for row in range(3):
+            cells = [
+                ' ' if board[row][col] is None else 'x' if board[row][col] == 0 else 'o'
+                for col in range(3)
+            ]
+            print(f' {cells[0]} | {cells[1]} | {cells[2]}')
+            if row < 2:
+                print('---+---+---')
+        print()
+        if self.is_terminal(state):
+            if self.utility(state, 0) > 0:
+                print(f'P1 won')
+            elif self.utility(state, 1) > 0:
+                print(f'P2 won')
+            else:
+                print('The game is a draw')
+        else:
+            print(f'It is P{self.to_move(state)+1}\'s turn to move')
+
+def alpha_beta_search(game:Game, state: State) -> Action:
+    player = game.to_move(state)
+    value, move = max_value(game,state,-math.inf,math.inf,player)
+    return move
+
+def max_value(game:Game, state:State,alpha:float, beta:float, player:int) -> tuple[int, Action | None]:
+    if game.is_terminal(state): return game.utility(state, player), None
+    v, move = -math.inf, None
+    for a in game.actions(state):
+        v2, a2 = min_value(game, game.result(state, a), alpha, beta, player)
+        if v2 > v:
+            v, move = v2, a
+            alpha = max(alpha, v)
+        if v >= beta: return v, move
+    return v, move
+
+def min_value(game:Game, state:State,alpha:float, beta:float, player:int) -> tuple[int, Action | None]:
+    if game.is_terminal(state): return game.utility(state, player), None
+    v, move = math.inf, None
+    for a in game.actions(state):
+        v2, a2 = max_value(game, game.result(state, a), alpha, beta, player)
+        if v2 < v:
+            v, move = v2, a
+            beta = min(beta, v)
+        if v <= alpha: return v, move
+    return v, move
+
+def main():
+    game = Game()
+
+    state = game.initial_state()
+    game.print(state)
+    while not game.is_terminal(state):
+        cur_player = game.to_move(state)
+        action = alpha_beta_search(game, state) # The player whose turn it is
+                                                # is the MAX player
+        print(f'P{cur_player+1}\'s action: {action}')
+        assert action is not None
+        state = game.result(state, action)
+        game.print(state)
+
+if __name__ == "__main__":
+    main()
